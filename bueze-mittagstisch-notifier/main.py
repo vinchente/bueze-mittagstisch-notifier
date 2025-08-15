96febd2c892c56782b54e25c1da547a68e7adc68
Vincent <vincent.evenschor@rwth-aachen.de>
2025-08-15 16:50:29 +0200

    test menu download

--- bueze-mittagstisch-notifier/main.py
+++ bueze-mittagstisch-notifier/main.py
@@ -1,5 +1,18 @@
+import httpx
+
+
 def main() -> None:
     print("Hello from bueze-mittagstisch-notifier!")
+    url = "https://bueze.de/wp-content/uploads/2025/08/Screenshot-2025-08-14-161035.png"
+
+    with httpx.Client() as client:
+        response = client.get(url)
+        response.raise_for_status()  # raise an error if download failed
+
+        with open("menu.png", "wb") as f:
+            f.write(response.content)
+
+    print("Menu downloaded.")
 
 
 if __name__ == "__main__":
