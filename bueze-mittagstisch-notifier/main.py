e642e265925c9fab339486100e1cf692e8603f59
Vincent Evenschor <vincent.evenschor@connect.shell>
2025-08-15 16:15:30 +0200

    add pyproject.toml, .gitignore, tasks.py and uv

---
+++ bueze-mittagstisch-notifier/main.py
@@ -0,0 +1,6 @@
+def main() -> None:
+    print("Hello from bueze-mittagstisch-notifier!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tasks.py
@@ -0,0 +1,18 @@
+from invoke import task, Collection, Context
+
+
+@task
+def test(ctx: Context) -> None:
+    ctx.run("ruff check")
+    ctx.run("ruff format --check")
+    ctx.run("mypy")
+    ctx.run("pytest -x --ff --cov=bueze-mittagstisch-notifier --cov-report=term --cov-report=xml")
+
+@task
+def fix(ctx: Context) -> None:
+    ctx.run("ruff check --fix")
+    ctx.run("ruff format")
+
+
+ns = Collection(test, fix)
+ns.configure({"run": {"echo": True, "pty": True}})
