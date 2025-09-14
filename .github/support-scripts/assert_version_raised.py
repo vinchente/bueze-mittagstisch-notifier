1c5d06afbff92dc232961799f0816b7e17e212ba
Vincent <vincent.evenschor@rwth-aachen.de>
2025-09-14 11:39:59 +0200

    fix script

--- .github/support-scripts/assert_version_raised.py
+++ .github/support-scripts/assert_version_raised.py
@@ -1,24 +1,32 @@
-import subprocess
+import json
 import tomllib
 import sys
+from pathlib import Path
 
-def get_branch_version_from_git(branch: str) -> tuple[int, ...]:
-    result = subprocess.run(
-        ["git", "show", f"{branch}:pyproject.toml"],
-        capture_output=True,
-        text=True,
-        check=True,
-    )
-    project_info = tomllib.loads(result.stdout)
-    return tuple(map(int, project_info["project"]["version"].split(".")))
+def get_branch_version(branch: Path) -> tuple[int, ...]:
+    pyproject_toml = branch / "pyproject.toml"
+    version_json = branch / "version.json"
 
-new_version = get_branch_version_from_git("HEAD")
-main_version = get_branch_version_from_git("origin/main")
+    project_info = tomllib.loads(pyproject_toml.read_text(encoding="utf-8"))
 
-for index, name in enumerate(["major", "minor", "patch"]):
-    if new_version[index] > main_version[index]:
+    if "project" in project_info:
+        return tuple(map(int, project_info["project"]["version"].split(".")))
+    else:
+        version_object = json.loads(version_json.read_text(encoding="utf-8"))
+        return (
+            version_object["major"],
+            version_object["minor"],
+            version_object["fix"],
+        )
+
+
+new_version_tuple = get_branch_version(Path("feature"))
+target_version_tuple = get_branch_version(Path("main"))
+
+for index, name in enumerate(["major", "minor", "fix"]):
+    if new_version_tuple[index] > target_version_tuple[index]:
         print(f"{name} version is raised")
         sys.exit(0)
 
-print("Version has NOT been bumped!")
+print("Versions are the same between current branch and main branch")
 sys.exit(1)
